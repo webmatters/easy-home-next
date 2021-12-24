dcfcde6f4d19f661f0c71cc50c42da5fae5be672
webmatters <bodhicoder@gmail.com>
2021-12-24 12:15:03 -0500

    Initial commit with hero and features.

---
+++ components/Features.jsx
@@ -0,0 +1,87 @@
+/* This example requires Tailwind CSS v2.0+ */
+import {
+  ShoppingBagIcon,
+  CogIcon,
+  CurrencyDollarIcon,
+  CreditCardIcon,
+  CalendarIcon,
+  HomeIcon,
+} from '@heroicons/react/outline';
+
+const features = [
+  {
+    name: 'One Stop Shop',
+    icon: ShoppingBagIcon,
+    text: 'No more asking around or juggling providers. Get it all from Easy Home.',
+  },
+  {
+    name: 'Easy Payment',
+    icon: CreditCardIcon,
+    text: 'No more invoices, checkbooks, or back and forth. Just sign up with your credit card.',
+  },
+  {
+    name: 'Convenient Scheduling',
+    icon: CalendarIcon,
+    text: "Set it and forget it. We'll send reminders for scheduled services prior to arriving.",
+  },
+  {
+    name: 'Full Transparency',
+    icon: CogIcon,
+    text: 'Manage your account. See completed and scheduled work. Cancel anytime.',
+  },
+  {
+    name: 'Quality Work',
+    icon: HomeIcon,
+    text: "Your satisfaction is guaranteed. The work is not done if you don't think it's done.",
+  },
+  {
+    name: 'Reasonable Rates',
+    icon: CurrencyDollarIcon,
+    text: 'Despite the added convenience and quality, our rates are comparable to small providers.',
+  },
+];
+
+export default function Features() {
+  return (
+    <div className="relative bg-white py-16 sm:py-24 lg:py-32">
+      <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+        <h2 className="text-base font-semibold tracking-wider text-blue-600 uppercase">
+          Live easier
+        </h2>
+        <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+          All-inclusive home care subscriptions
+        </p>
+        <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+          Pay one monthly fee and never have to worry about doing those
+          time-consuming, energy-draining chores again.
+        </p>
+        <div className="mt-12">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            {features.map(feature => (
+              <div key={feature.name} className="pt-6">
+                <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                  <div className="-mt-6">
+                    <div>
+                      <span className="inline-flex items-center justify-center p-3 bg-blue-500 rounded-md shadow-lg">
+                        <feature.icon
+                          className="h-6 w-6 text-white"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </div>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                      {feature.name}
+                    </h3>
+                    <p className="mt-5 text-base text-gray-500">
+                      {feature.text}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/Hero.jsx
@@ -0,0 +1,81 @@
+export default function Hero() {
+  return (
+    <div className="bg-white pb-8 sm:pb-12 lg:pb-12">
+      <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-48">
+        <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
+          <div>
+            <div>
+              <img
+                className="h-11 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=blue&shade=500"
+                alt="Workflow"
+              />
+            </div>
+            <div className="mt-20">
+              <div className="mt-6 sm:max-w-xl">
+                <h1>
+                  <span className="block text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
+                    Coming soon
+                  </span>
+                  <span className="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl">
+                    <span className="block text-blue-600">Easy Home</span>
+                    <span className="block text-gray-900">
+                      Home Care Packages
+                    </span>
+                  </span>
+                </h1>
+                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                  Ever wish you could just sign up and pay one monthly fee to
+                  have all of your home maintenance chores handled for you. With
+                  Easy Home you'll be able to. Just select the package of
+                  services you want, how often you want them done, and we'll
+                  take care of the rest. Combine services like lawn care, house
+                  cleaning, grocery shopping, pet care, and more.
+                </p>
+                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                  Enter your email below to be notified when we launch.
+                </p>
+              </div>
+              <form action="#" className="mt-12 sm:max-w-lg sm:w-full sm:flex">
+                <div className="min-w-0 flex-1">
+                  <label htmlFor="hero-email" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="hero-email"
+                    type="email"
+                    className="block w-full border border-gray-300 rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                    placeholder="Enter your email"
+                  />
+                </div>
+                <div className="mt-4 sm:mt-0 sm:ml-3">
+                  <button
+                    type="submit"
+                    className="block w-full rounded-md border border-transparent px-5 py-3 bg-blue-600 text-base font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10"
+                  >
+                    Notify me
+                  </button>
+                </div>
+              </form>
+              <p className="mt-3 text-sm text-gray-500">
+                We care about the protection of your data. Read our{' '}
+                <a href="#" className="font-medium text-gray-900 underline">
+                  Privacy Policy
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+          <img
+            className="absolute inset-0 w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1549517045-bc93de075e53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
